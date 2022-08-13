28df7d965ac0dbd9832243d09bca9eee32cbe835
Daniel <61473070+heboq@users.noreply.github.com>
2022-08-13 20:26:04 +0700

    change solution
    Change solution to get predictable order of elements (Users) in the returned slice from GetBatch func. Make getBatch func and User struct exportable for manual testing purpose.

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -5,18 +5,19 @@ import (
 	"time"
 )
 
-type user struct {
+type User struct {
 	ID int64
 }
 
-func getOne(id int64) user {
+func getOne(id int64) User {
 	time.Sleep(time.Millisecond * 100)
-	return user{ID: id}
+	return User{ID: id}
 }
 
-func getBatch(n int64, pool int64) (res []user) {
+func GetBatch(n int64, pool int64) []User {
 	var wg sync.WaitGroup
 	var mu sync.Mutex
+	res := make([]User, n)
 	sem := make(chan struct{}, pool)
 	wg.Add(int(n))
 	for i := int64(0); i < n; i++ {
@@ -25,11 +26,12 @@ func getBatch(n int64, pool int64) (res []user) {
 			sem <- struct{}{}
 			user := getOne(i)
 			mu.Lock()
-			res = append(res, user)
+			// // res = append(res, user)
+			res[i] = user
 			mu.Unlock()
 			<-sem
 		}(i)
 	}
 	wg.Wait()
-	return
+	return res
 }
